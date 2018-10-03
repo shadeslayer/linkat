5b2f684082fa155ab381e2e40046918f4ed19b3c
Rohan Garg <rohan@garg.io>
2018-10-03 19:23:16 +0200

    Add xorriso

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ apt-get -y install software-properties-common
 apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 55751E5D
 add-apt-repository "deb http://archive.neon.kde.org/dev/stable bionic main"
 apt update
-apt-get -y install live-build syslinux memtest86+ genisoimage mtools syslinux-utils
+apt-get -y install live-build syslinux memtest86+ genisoimage mtools syslinux-utils xorriso
 cd /build/
 lb clean
 ./configure
