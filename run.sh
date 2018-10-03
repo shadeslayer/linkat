33c552d7eaab470dfe22c3c83e2a6b9c1bf8c9a3
Rohan Garg <rohan@garg.io>
2018-10-03 22:47:02 +0200

    Add dirmngr

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 apt-get update
-apt-get -y install software-properties-common
+apt-get -y install software-properties-common dirmngr
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 55751E5D
 add-apt-repository "deb http://archive.neon.kde.org/dev/stable bionic main"
 apt update
