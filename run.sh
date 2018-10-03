733940a592d03b0b7879af12608362d61a5cb548
Rohan Garg <rohan@garg.io>
2018-10-03 22:45:16 +0200

    Use the ubuntu keyserver

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 apt-get update
 apt-get -y install software-properties-common
-apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 55751E5D
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 55751E5D
 add-apt-repository "deb http://archive.neon.kde.org/dev/stable bionic main"
 apt update
 apt-get -y install live-build syslinux memtest86+ genisoimage mtools syslinux-utils xorriso
