5a0bff7f3b0fff51bd4caa94b864a018bfdc93d5
Rohan Garg <rohan@garg.io>
2018-10-04 11:30:04 +0200

    Build without chroot, install everything on the host, ensure stuff works

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ apt-get update
 apt-get -y install software-properties-common dirmngr
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 55751E5D
 add-apt-repository "deb http://archive.neon.kde.org/dev/stable bionic main"
-apt-get -y install live-build syslinux memtest86+ genisoimage mtools syslinux-utils xorriso
+apt-get -y install live-build syslinux memtest86+ genisoimage mtools syslinux-utils xorriso syslinux-utils dctrl-tools squashfs-tools syslinux-themes-neon gfxboot-theme-ubuntu zsync
 cd /build/
 lb clean
 ./configure
